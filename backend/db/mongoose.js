50dcdd3bc0aa7d5761a4292aea7ad56ca2821007
SaiBalaji <tenkatibalaji29@gmail.com>
2025-04-10 11:55:51 +0530

    changes

--- backend/db/mongoose.js
+++ backend/db/mongoose.js
@@ -1,10 +1,11 @@
 // backend/config/mongoose.js
 const mongoose = require('mongoose');
-const MONGO_URI="mongodb+srv://SaiBalaji:SaiBalajiqwerty11@cluster0.d7rbrg0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0/bloodDonation"
+require('dotenv').config(); // Make sure environment variables are loaded
+
 const connectDB = async () => {
   try {
-    await mongoose.connect(MONGO_URI);
-    console.log('MongoDB connected successfully');
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+    console.log(`MongoDB connected successfully`);
   } catch (err) {
     console.error('MongoDB connection failed:', err.message);
     process.exit(1); 
