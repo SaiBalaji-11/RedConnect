2d7a1effa239f2d10df30f87dd109998d45b0b76
Sai Balaji <tenkatibalaji29@gmail.com>
2025-06-22 13:15:32 +0530

    Fix: corrected frontend issue

--- RedConnect/frontend/src/components/Signup.jsx
+++ RedConnect/frontend/src/components/Signup.jsx
@@ -129,20 +129,20 @@ export default function Signup() {
         </div>
 
         {/* Image Upload */}
-        <div className="flex flex-col">
-  <label className="text-blue-900 font-medium mb-1">Upload Image</label>
+ <div className="flex flex-col">
+      <label className="text-blue-900 font-medium mb-1">Upload Image</label>
 
-  <label className="inline-block bg-blue-600 hover:bg-blue-700 text-white px-4 py-2 rounded cursor-pointer w-fit">
-    Choose File
-    <input
-      name="image"
-      type="file"
-      accept="image/*"
-      onChange={handleChange}
-      className="hidden"
-    />
-  </label>
-</div>
+      <label className="bg-blue-600 hover:bg-blue-700 text-white px-4 py-2 rounded cursor-pointer w-fit text-center">
+        {fileName}
+        <input
+          name="image"
+          type="file"
+          accept="image/*"
+          onChange={onFileChange}
+          className="hidden"
+        />
+      </label>
+    </div>
 
 
         <button type="submit" className="bg-red-500 text-white py-2 px-4 rounded-xl hover:bg-red-600">
